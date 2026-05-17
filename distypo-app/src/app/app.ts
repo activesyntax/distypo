547a7aecfc1430bc0a788d7db62a22b6da51aa74
Active Syntax <codemaster@activesyntax.net>
2026-05-17 13:53:01 +0200

    toolbar with logo

--- distypo-app/src/app/app.ts
+++ distypo-app/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Workspace } from './workspace/workspace';
+import { MatToolbar } from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Workspace],
+  imports: [RouterOutlet, MatToolbar, Workspace],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('distypo-app');
