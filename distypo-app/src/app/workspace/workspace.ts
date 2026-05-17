f09de53fd4f0e364c7bd27705fce9ca002beec9e
Active Syntax <codemaster@activesyntax.net>
2026-05-17 18:46:05 +0200

    document component added

--- distypo-app/src/app/workspace/workspace.ts
+++ distypo-app/src/app/workspace/workspace.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Document } from '../document/document';
 
 @Component({
   selector: 'app-workspace',
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, Document],
   templateUrl: './workspace.html',
   styleUrl: './workspace.css',
 })
