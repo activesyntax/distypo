8e4fcf32616adf78d2ab18eec42b1cc14e43ccfd
Active Syntax <codemaster@activesyntax.net>
2026-05-23 12:42:12 +0200

    rules have been extracted to config/rules

---
+++ distypo-app/src/config/rules.ts
@@ -0,0 +1,18 @@
+import { Rule } from "@core/domain/rules";
+
+export const lowercaseSentenceStartRule: Rule = {
+  name: 'lowercase-sentence-start',
+  description: 'Detects sentences that start with a lowercase letter after punctuation.',
+  regex: /([.!?])(\s+)(\p{Ll})/gu,
+  corrector: (match: RegExpMatchArray) => {
+    const punctuation = match[1];
+    const whitespace = match[2];
+    const lowercaseLetter = match[3];
+    return `${punctuation}${whitespace}${lowercaseLetter.toUpperCase()}`;
+  }
+};
+
+export const rules: Rule[] = [
+  lowercaseSentenceStartRule,
+];
+
--- distypo-app/src/core/operations/lint.ts
+++ distypo-app/src/core/operations/lint.ts
@@ -2,23 +2,8 @@ import { Correction, LintedDocument, RawDocument } from "@core/domain/model";
 import { Rule } from "@core/domain/rules";
 import { TextRange } from "@core/domain/text-range";
 import { createGuid } from "@core/utils";
+import { rules } from "@config/rules";
 
-
-export const lowercaseSentenceStartRule: Rule = {
-  name: 'lowercase-sentence-start',
-  description: 'Detects sentences that start with a lowercase letter after punctuation.',
-  regex: /([.!?])(\s+)(\p{Ll})/gu,
-  corrector: (match: RegExpMatchArray) => {
-    const punctuation = match[1];
-    const whitespace = match[2];
-    const lowercaseLetter = match[3];
-    return `${punctuation}${whitespace}${lowercaseLetter.toUpperCase()}`;
-  }
-};
-
-const rules: Rule[] = [
-  lowercaseSentenceStartRule,
-];
 
 export function lint(doc: RawDocument): LintedDocument {
 
