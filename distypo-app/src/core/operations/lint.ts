fa8c15783b44849ac034f7afc299af166408e08f
Active Syntax <codemaster@activesyntax.net>
2026-05-23 10:29:21 +0200

    createGuid added + sytax fix

--- distypo-app/src/core/operations/lint.ts
+++ distypo-app/src/core/operations/lint.ts
@@ -1,6 +1,7 @@
 import { Correction, LintedDocument, RawDocument } from "@core/domain/model";
 import { Rule } from "@core/domain/rules";
 import { TextRange } from "@core/domain/text-range";
+import { createGuid } from "@core/utils";
 
 
 export const lowercaseSentenceStartRule: Rule = {
@@ -16,15 +17,26 @@ const rules: Rule[] = [
 
 export function lint(doc: RawDocument): LintedDocument {
 
-  const corrections: Correction[] = [];
+  const corrections: Correction[] = getCorrections(doc, rules);
 
-  const matches = rules.flatMap(rule => {
-    return [...doc.content.matchAll(rule.regex)].map(matchToTextRange);
-  });
+  // const corrections: Correction[] = rules.flatMap(rule => {
+  //   const ranges = [...doc.content.matchAll(rule.regex)].map(matchToTextRange);
+  //
+  //   return ranges.map(range => { rangeToCorrection(range, rule) }
+  //   );
+  // }
+  // );
 
-  const issues = matches;
+  // const corrections: Correction[] =
+  //   matches.map(range => ({
+  //
+  //     id: createGuid("CorrectionId"),
+  //     range,
+  //     replacement: doc.content.slice(range.start, range.end).toUpperCase(), // example replacement logic
+  //   }));
+  //
 
-  console.log('Issues found:', issues);
+  // console.log('Issues found:', issues);
 
   const lintedDoc: LintedDocument = {
     kind: "linted",
@@ -35,10 +47,19 @@ export function lint(doc: RawDocument): LintedDocument {
   return lintedDoc;
 }
 
+function getCorrections(doc: RawDocument, rules: Rule[]): Correction[] {
+  const corrections: Correction[] = [];
+
+  return [];
+}
+
 function matchToTextRange(match: RegExpMatchArray): TextRange {
   return {
     start: (match.index || 0),
     end: (match.index || 0) + match[0].length,
     __brand: 'TextRange',
   }
+}
+
+function rangeToCorrection(range: unknown, rule: Rule) {
 }
