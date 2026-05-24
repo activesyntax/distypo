2a38b5ee969b79d54a8ea75ab9ed0f55898ab114
Active Syntax <codemaster@activesyntax.net>
2026-05-24 18:39:54 +0200

    segmentation experiments

--- distypo-app/src/app/document-view/services/segmentation-service.ts
+++ distypo-app/src/app/document-view/services/segmentation-service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Correction, LintedDocument } from "@core/index";
+import { pairwise } from '@core/utils';
 
 export type Segment =
   | { kind: 'text'; text: string }
   | { kind: 'correction'; correction: Correction };
+
+type SegmentKind = "text" | "correction";
+
+type SegmentBoundary = [number, number];
+type SegmentMap = [SegmentKind, SegmentBoundary];
 
 @Injectable()
 export class SegmentationService {
@@ -11,9 +17,34 @@ export class SegmentationService {
   split(document: LintedDocument): Segment[] {
 
     const sortedCorrections = document.corrections.toSorted((a: Correction, b: Correction) => a.range.start - b.range.start);
+    const splitPoints = sortedCorrections.flatMap(c => [c.range.start, c.range.end]);
 
-    const segments: Segment[] = [];
+    console.log("Split points", splitPoints);
 
+
+    const segmentMap: SegmentMap[] = [...pairwise(splitPoints)].map(
+      ([start, end], i): SegmentMap => [
+        i % 2 === 0 ? 'correction' : 'text',
+        [start, end],
+      ]
+    );
+
+    console.log("Segment map", segmentMap);
+
+    const boindaries = [...pairwise(splitPoints)];
+
+    console.log("Boundaries", boindaries);
+
+    const segments: Segment[] = [...pairwise(splitPoints)]
+      .map(([start, end], i): Segment =>
+        i % 2 === 0
+          ? { kind: 'correction', correction: document.corrections[i] }
+          : { kind: 'text', text: document.content.slice(start, end) }
+      )
+      .filter(seg => seg.kind === 'correction' || seg.text.length > 0);
+
+
+    console.log("Segments", segments);
     return segments;
   }
 }
