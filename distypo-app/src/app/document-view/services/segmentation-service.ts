829cf34eb709168502e11dba7a2df091facf6e6b
Active Syntax <codemaster@activesyntax.net>
2026-05-25 16:40:38 +0200

    more readable split

--- distypo-app/src/app/document-view/services/segmentation-service.ts
+++ distypo-app/src/app/document-view/services/segmentation-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Correction, LintedDocument } from "@core/index";
-import { complement, Interval } from '@core/utils';
+import { complement, interval, Interval } from '@core/utils';
 
 export type Segment =
   | { kind: 'text'; text: string, range: Interval }
@@ -11,25 +11,29 @@ export type Segment =
 export class SegmentationService {
 
   split(document: LintedDocument): Segment[] {
-    const correctionSegments: Segment[] = document.corrections.map(c => ({
-      kind: 'correction',
-      correction: c,
-      range: c.range,
-    }));
+    const correctionSegments: Segment[] = document.corrections.map(toCorrectionSegment);
 
     const gaps = complement(
       document.corrections.map(c => c.range),
-      { start: 0, end: document.content.length },
+      interval(0, document.content.length)
     );
 
-    const textSegments: Segment[] = gaps.map(range => ({
-      kind: 'text',
-      text: document.content.slice(range.start, range.end),
-      range,
-    }));
+    const textSegments: Segment[] = gaps.map(range => toTextSegment(range, document.content.slice(range.start, range.end)));
 
     return [...correctionSegments, ...textSegments].toSorted(
       (a, b) => a.range.start - b.range.start
     );
   }
 }
+
+const toCorrectionSegment = (c: Correction): Segment => ({
+  kind: 'correction',
+  correction: c,
+  range: c.range,
+});
+
+const toTextSegment = (range: Interval, text: string): Segment => ({
+  kind: 'text',
+  text: text,
+  range,
+});
