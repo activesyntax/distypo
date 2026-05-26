ea7f648677cb5a192814806512f33e392663a9ea
Active Syntax <codemaster@activesyntax.net>
2026-05-26 11:12:29 +0200

    correciton-view first code

--- distypo-app/src/app/correction-view/correction-view.ts
+++ distypo-app/src/app/correction-view/correction-view.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Correction } from '@core/index';
 
 @Component({
   selector: 'app-correction-view',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './correction-view.html',
   styleUrl: './correction-view.scss',
 })
-export class CorrectionView {}
+export class CorrectionView {
+  readonly correction = input.required<Correction>();
+  readonly selected = input<boolean>(false);
+
+  readonly select = output<void>();
+  readonly keep = output<void>();
+  readonly fix = output<void>();
+  readonly edit = output<void>();
+}
