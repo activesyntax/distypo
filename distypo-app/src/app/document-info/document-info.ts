85e56c27865c17f2d7017d790d91968dfc6d753e
Active Syntax <codemaster@activesyntax.net>
2026-05-30 14:35:34 +0200

    document info finished

--- distypo-app/src/app/document-info/document-info.ts
+++ distypo-app/src/app/document-info/document-info.ts
@@ -1,6 +1,7 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { DocumentState } from '@app/state/document-state';
+import { formatBytes } from '@core/utils/format';
 
 @Component({
   selector: 'app-document-info',
@@ -9,17 +10,7 @@ import { DocumentState } from '@app/state/document-state';
   styleUrl: './document-info.scss',
 })
 export class DocumentInfo {
-
   documentState = inject(DocumentState);
-
-  filename = signal<string | undefined>('intro.txt');
-
   formattedSize = computed(() => formatBytes(this.documentState.contentSizeBytes()));
-  hasFile = computed(() => this.filename() !== null);
 }
 
-function formatBytes(bytes: number): string {
-  if (bytes < 1024) return `${bytes} B`;
-  if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(1)} KB`;
-  return `${(bytes / (1024 * 1024)).toFixed(1)} MB`;
-}
