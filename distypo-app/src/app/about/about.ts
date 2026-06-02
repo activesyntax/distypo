9b0482c21f6fc4c44c23dcd91e19dd98e00111c7
Active Syntax <codemaster@activesyntax.net>
2026-06-02 23:02:26 +0200

    about page added

--- distypo-app/src/app/about/about.ts
+++ distypo-app/src/app/about/about.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
-export class About {}
+export class About { }
